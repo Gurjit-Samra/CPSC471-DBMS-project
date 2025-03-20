14e903b8b0c923c4f113ce78b4abbf60b02cce48
farisalhi <faris1.salhi@gmail.com>
2025-03-20 12:45:23 -0600

    Connected django to mySQL.

--- tech_storefront/tech_storefront/settings.py
+++ tech_storefront/tech_storefront/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'tech_storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'techstore',
+        'USER': 'root',
+        'PASSWORD': 'Pp#J^kl%XW8So8',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
